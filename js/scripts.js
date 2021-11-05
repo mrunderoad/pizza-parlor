a0df44cc15278d63199216ead08dee7c4b107800
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-11-05 13:53:41 -0700

    figure it out after a break. struggling

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,46 @@
-function Pizza(toppingOne, toppingTwo, size) {
+function Pizza(cost, size, toppingOne, toppingTwo) {
   this.toppingOne = toppingOne;
   this.toppingTwo = toppingTwo;
   this.size = size;
-  this.cost = 15;
+  this.cost = 0;
 }
 
-Pizza.prototype.createPizza = function(){
+Pizza.prototype.createCost = function(){
   if (this.size === "small") {
-    this.cost -= 2;
+    this.cost = 13;
   } else if (this.size === "large") {
-    this.cost += 2;
-  } else {
-  this.cost;
+    this.cost = 17;
+  } else if (this.size === "medium") {
+    this.cost = 15;
+  }
 }
 
-  if (this.toppingOne === "topping") {
-    this.cost + 1;
-  } else if (this.toppingTwo === "topping") {
-    this.cost + 1;
+  if (this.toppingOne === "topping1") {
+    this.cost += 1;
+  } else if (this.toppingTwo === "topping2") {
+    this.cost += 1;
   } else {
-  this.cost;
+  this.cost + 2;
 }
 return this.cost;
-};
+
+function displayCost() {
+  $("#cost").text(this.cost);
+  $("#topping1").text();
+  $("#topping2").text();
+}
+
+$(document).ready(function() {
+  $("form#pizza-size").submit(function(event) {
+    event.preventDefault();
+    let size = $("input:radio[name=crust]:checked").val();
+    let toppingOne = $("input:radio[name=topping1]:checked").val();
+    let toppingTwo = $("input:radio[name=topping2]:checked").val();
+    $("input:radio[name=crust]:checked").val("");
+    $("input:radio[name=topping1]:checked").val("");
+    $("input:radio[name=topping2]:checked").val("");
+    let userPizza = new Pizza(size, cost, toppingOne, toppingTwo)
+    userPizza.createCost();
+    console.log(userPizza.cost);
+  });
+})
